690b3b4b9419bdc4b5e5c321accc79e02fb814f5
Weber <xswzaq44321@gmail.com>
2018-12-01 01:34:14 +0800

    Add menu scene

---
+++ Assets/Script/button_exit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class button_exit : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Application.Quit();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
