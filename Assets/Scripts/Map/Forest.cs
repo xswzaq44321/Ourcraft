c3611232438298938f14354d3825ba93de28e1bf
IzsKon <eric246871@gmail.com>
2018-12-08 02:41:07 +0800

    map generator modularization

---
+++ Assets/Scripts/Map/Forest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forest : MonoBehaviour {
+
+    public Transform MainLand;
+    public Vector2 rangeX, rangeZ, rangeHeight;
+    // Use this for initialization
+    void Start () {
+        for (float i = 0; i < 100; i++)
+        {
+            float PosX = Random.Range(rangeX.x, rangeX.y) + 0.5f, PosY = Random.Range(rangeZ.x, rangeZ.y) + 0.5f;
+            for (float j = Random.Range(rangeHeight.x, rangeHeight.y) + 0.5f; j > 0; j--)
+            {
+                GameObject forest = Instantiate(Resources.Load("blocks/log_oak") as GameObject);
+                forest.transform.position = new Vector3(PosX, j + MainLand.transform.position.y, PosY);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Map/Ground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+    public Transform MainLand;
+    private List<string> material = new List<string>();
+    public bool grass, sand, dirt, stone;
+    public Vector2 rangeX, rangeZ;
+
+    // Use this for initialization
+    void Start () {
+        if (grass) material.Add("grass");
+        if (sand) material.Add("sand");
+        if (dirt) material.Add("dirt");
+        if (stone) material.Add("stone");
+        for(float i = rangeX.x + 0.5f; i < rangeX.y; i++)
+            for (float j = rangeZ.x + 0.5f; j < rangeZ.y; j++)
+            {
+                GameObject block = Instantiate(Resources.Load("blocks/" + material[Random.Range(0, material.Count - 1)]) as GameObject);
+                block.transform.position = new Vector3(i, MainLand.transform.position.y + block.transform.localScale.y / 2, j);
+            }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
