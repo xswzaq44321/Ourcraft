c7728b2c6774a6e4883f956c3531cb8d1128eb28
Weber <xswzaq44321@gmail.com>
2018-12-08 02:59:25 +0800

    Add monsterGenObj & player animation

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -44,5 +44,6 @@ public class MonsterController : MonoBehaviour {
 			HP = MAX_HP;
 		else if (HP < 0)
 			Destroy(this);
+		GetComponent<Rigidbody>().AddForce((Vector3.back + Vector3.up)*200);
 	}
 }
---
+++ Assets/Scripts/MonsterGen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGen : MonoBehaviour {
+
+	public GameObject player;
+	public Light sun;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(sun.transform.eulerAngles.x);
+	}
+}
