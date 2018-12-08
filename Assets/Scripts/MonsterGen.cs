64707cbdb2b6e566b67b351006f7bf1484fe804b
Weber <xswzaq44321@gmail.com>
2018-12-08 16:33:42 +0800

    Adjusting monster generation
    adjusting min, max range
    adjusting monster despawn behavior

--- Assets/Scripts/MonsterGen.cs
+++ Assets/Scripts/MonsterGen.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterGen : MonoBehaviour {
+public class MonsterGen : MonoBehaviour
+{
 
 	public GameObject player;
 	public Light sun;
@@ -12,34 +13,39 @@ public class MonsterGen : MonoBehaviour {
 	private float time;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		Vector3 dist = new Vector3(1, 0, 0);
 		Vector3 endPoint;
 		time += Time.deltaTime;
-		if(sun.transform.eulerAngles.x > 270f && time > 1f)
-		{
-			Debug.Log("party time!");
+		if (sun.transform.eulerAngles.x > 270f && time > 1f)
+		{ // night time
 			time = 0;
+			// decide where and how far from player that the monsters'll spawn
 			dist *= Random.Range(minRange, maxRange);
 			dist = Quaternion.Euler(0, Random.Range(0, 360), 0) * dist;
 			endPoint = player.transform.position + dist;
+			// spawn only on ground
 			endPoint.y = MainLand.transform.position.y;
 			GameObject enemy = Instantiate(monster);
 			enemy.transform.parent = this.transform;
-			enemy.GetComponent<MonsterController>().player = player;
 			enemy.transform.position = endPoint;
+			// target lock to player
+			enemy.GetComponent<MonsterController>().player = player;
 		}
-		else if(sun.transform.eulerAngles.x < 90)
-		{
-			for(int i = 0; i < this.transform.childCount; ++i)
+		else if(sun.transform.eulerAngles.x < 90f)
+		{ // day time
+			if (this.transform.childCount > 0)
 			{
-				Destroy(this.transform.GetChild(i).gameObject);
+				// destroy evil viking one by one
+				Destroy(this.transform.GetChild(0).gameObject);
+				this.transform.GetChild(0).parent = null;
 			}
-			this.transform.DetachChildren();
 		}
 	}
 }
