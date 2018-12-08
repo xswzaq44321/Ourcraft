063944b5d20ca851ada623b6e3131bbae85ae642
IzsKon <eric246871@gmail.com>
2018-12-08 14:34:43 +0800

    add game introduction
    still many bugs, but not unbearable, maybe I'll just leave it like that?

---
+++ Assets/Scripts/button_help.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class button_help : MonoBehaviour, IPointerClickHandler
+{
+
+    public GameObject introduction;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        introduction.gameObject.SetActive(true);
+    }
+    // Use this for initialization
+    void Start () {
+        introduction.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
